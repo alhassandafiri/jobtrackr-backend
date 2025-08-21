c1c8762e92622e2b2f640ab4103e480c83fcc641
Harson <hassandaf07@gmail.com>
2025-08-21 14:51:45 +0100

    add user retrieval endpoint and clean up route formatting

--- routes/api.php
+++ routes/api.php
@@ -2,9 +2,11 @@
 
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 Route::post('/register', [AuthController::class, 'register']);
-
 Route::post('/login', [AuthController::class, 'login']);
-
 Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
