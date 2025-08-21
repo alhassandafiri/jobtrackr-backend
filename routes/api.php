29856a83b69bf6e3cb89d150a88c2f9026e433b0
Harson <hassandaf07@gmail.com>
2025-08-21 18:23:09 +0100

    reformat api.php

--- routes/api.php
+++ routes/api.php
@@ -5,8 +5,11 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 
 Route::post('/register', [AuthController::class, 'register']);
+
 Route::post('/login', [AuthController::class, 'login']);
+
 Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
