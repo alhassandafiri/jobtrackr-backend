7f69c5b0820d0622da449b4f84791407a6295e0f
Harson <hassandaf07@gmail.com>
2025-08-24 20:14:53 +0100

    implement index method to retrieve and return user's job applications

--- app/Http/Controllers/JobApplicationController.php
+++ app/Http/Controllers/JobApplicationController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\JobApplication;
 use Illuminate\Http\Request;
 
 class JobApplicationController extends Controller
 {
-    //
+    public function index(Request $request) {
+        $jobs = JobApplication::where('/user_id', $request->user()->id)
+        ->orderBy('created_at', 'desc')
+        ->get();
+
+        return response()->json($jobs);
+    }
 }
